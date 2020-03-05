b8a1e6e2196d9336f42d851982e6ff308519be03
Maria Sevostyanova <letanina.maria@gmail.com>
2020-03-05 20:17:48 +0400

    Добавление теста

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,23 @@
 import org.junit.jupiter.api.Test;
 
-class CardDeliveryTest {
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
+class CardDeliveryTest {
     @Test
+    void shouldRegisterDeliveryTest() {
+        open ("http://localhost:9999");
+        $$("[type=text]").first().setValue("Самара");
+        $("span.menu-item__control").click();
+        $$("[type='tel']").first().setValue("09.03.2020");
+        $("[name=name]").setValue("Петров Алексей");
+        $$("[type='tel']").last().setValue("+79877899999");
+        $("span.checkbox__box").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+    }
+
+
+
 }
