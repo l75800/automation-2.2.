3916bceb4fd37b2ee427d42f183f4f5da20e1c3f
Maria Sevostyanova <letanina.maria@gmail.com>
2020-03-12 09:11:42 +0400

    Добавление корректировок

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -10,14 +13,13 @@ class CardDeliveryTest {
         open ("http://localhost:9999");
         $$("[type=text]").first().setValue("Самара");
         $("span.menu-item__control").click();
-        $$("[type='tel']").first().setValue("09.03.2020");
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(3);
+        $("[placeholder='Дата встречи']").setValue(String.valueOf(date));
         $("[name=name]").setValue("Петров Алексей");
         $$("[type='tel']").last().setValue("+79877899999");
         $("span.checkbox__box").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
     }
-
-
-
 }
